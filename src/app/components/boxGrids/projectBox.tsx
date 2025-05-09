60718391b09690185b71f22f5798ba0a95d14664
Jayden Tan <jaydentan@169-231-81-247.wireless.ucsb.edu>
2025-05-08 20:49:07 -0700

    connected projects

--- src/app/components/boxGrids/projectBox.tsx
+++ src/app/components/boxGrids/projectBox.tsx
@@ -4,51 +4,92 @@ import { useTheme } from "@/app/context/themecontext";
 import { useInView } from "react-intersection-observer";
 import Link from "next/link";
 
-export default function Projectbox(){
-    
-    const [isTransitioning, setIsTransitioning] = useState(true);
+export interface ProjectboxProps {
+    name: string;
+    date: string;
+    description: string;
+    techs: string[];
+    url: string;
+}
 
-    const { ref, inView } = useInView({
-        threshold: 0.1,
-    });
+export default function Projectbox({ name, date, description, techs, url }: ProjectboxProps) {
+    const [isTransitioning, setIsTransitioning] = useState(true);
+    const { ref, inView } = useInView({ threshold: 0.1 });
+    const { isDarkMode } = useTheme();
 
     useEffect(() => {
-        if (inView){
-            setIsTransitioning(false);
-        } else {
-            setIsTransitioning(true);
-        }
+        setIsTransitioning(!inView);
     }, [inView]);
-    const {isDarkMode} = useTheme();
-    const technologies = ["Tech", "Tech", "Tech", "Tech"];
 
     return (
-        <div ref={ref} className={`box-border border-4 rounded-sm max-w-[47vw] ${isDarkMode ? "border-white" : "border-black"} ${isTransitioning ? 'opacity-0 scale-95' : 'opacity-100 scale-100'} transition-all duration-600`}>
-            <Link href="https://github.com/jaytan3966" target="blank">
-                <img title="Nyan Shibar"
-                src="https://no-cdn.shortpixel.ai/client/to_avif,q_lossy,ret_wait/https://shortpixel.com/blog/wp-content/uploads/2023/12/nyan-cat.gif"
-                className="w-full hover:opacity-30 hover:cursor-pointer transition-all duration-500"/> 
-            </Link>
-            <div className="mx-3 my-2 font-bold flex items-end text-center">
-                <h1 className="text-lg md:text-xl">{"<"}Nyan Shibar id="Personal Project"{">"}</h1>
-            </div>
-            
-            <p className={`text-center mb-2 mx-2 ${isDarkMode ? "text-gray-700" : "text-gray-500"} transition-all duration-500`}>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-            <div>
-                {technologies.map((name, i) => {
-                    return (
-                        <button 
-                        className={`mx-3 my-2 px-2 py-1 box-border rounded-xl ${isDarkMode ? "bg-gray-800 text-gray-250" : "bg-gray-300 text-gray-800"} text-xs transition-all duration-500 hover:cursor-pointer`}
-                        key={i}
-                        >
-                            {"<"}{name} {i+1}{"/>"}
-                        </button>
+        <div 
+            ref={ref}
+            className={`box-border border-4 flex flex-col rounded-sm max-w-[47vw] h-full ${
+                isDarkMode ? "border-white" : "border-black"
+            } ${
+                isTransitioning ? 'opacity-0 scale-95' : 'opacity-100 scale-100'
+            } transition-all duration-600`}
+        >
+            <Link 
+                href={`${url}`} 
+                target="_blank" 
+                className="flex-shrink-0 relative group overflow-hidden"
+                >
+                <div className="relative">
+                    <img 
+                    title="View this project"
+                    src="https://no-cdn.shortpixel.ai/client/to_avif,q_lossy,ret_wait/https://shortpixel.com/blog/wp-content/uploads/2023/12/nyan-cat.gif"
+                    className="w-full group-hover:opacity-30 hover:cursor-pointer transition-all duration-500"
+                    alt={name}
+                    />
                     
-                    )
-                })}
-            </div>
-            <div className="mx-3 my-3 text-lg font-bold md:text-xl">
-                <h1>{"</"}Nyan Shibar{">"}</h1>
+
+                    <div className="absolute inset-0 flex items-center justify-center opacity-0 group-hover:opacity-100 transition-opacity duration-300 pointer-events-none">
+                    <svg 
+                        className="w-12 h-12 bg-black/50 rounded-full p-2 text-white"
+                        fill="none" 
+                        stroke="currentColor" 
+                        viewBox="0 0 24 24"
+                        xmlns="http://www.w3.org/2000/svg"
+                    >
+                        <path 
+                        strokeLinecap="round" 
+                        strokeLinejoin="round" 
+                        strokeWidth={2} 
+                        d="M10 6H6a2 2 0 00-2 2v10a2 2 0 002 2h10a2 2 0 002-2v-4M14 4h6m0 0v6m0-6L10 14" 
+                        />
+                    </svg>
+                    </div>
+                </div>
+            </Link>
+            
+            <div className="flex-grow flex flex-col p-2">
+                <div className="font-bold">
+                    <h1 className="text-lg md:text-xl">{"<"}{name} created="{date}"{">"}</h1>
+                </div>
+                
+                <div className="flex-grow min-h-[60px] my-2 overflow-y-auto">
+                    <p className={`text-center ${isDarkMode ? "text-gray-400" : "text-gray-600"}`}>
+                        {description}
+                    </p>
+                </div>
+                
+                <div className="flex flex-wrap gap-x-5 gap-y-2 m-3">
+                    {techs.map((tech, i) => (
+                        <button 
+                            className={`px-2 py-1 rounded-xl text-xs ${
+                                isDarkMode ? "bg-gray-800 text-gray-250" : "bg-gray-300 text-gray-800"
+                            } transition-all duration-500 hover:cursor-pointer`}
+                            key={i}
+                        >
+                            {"<"}{tech}{"/>"}
+                        </button>
+                    ))}
+                </div>
+                
+                <div className="font-bold">
+                    <h1 className="text-md lg:text-xl">{"</"}{name}{">"}</h1>
+                </div>
             </div>
         </div>
     )
--- src/app/components/boxGrids/projectsGrid.tsx
+++ src/app/components/boxGrids/projectsGrid.tsx
@@ -1,16 +1,67 @@
 "use client"
 import Projectbox from "./projectBox"
+import { useEffect, useState } from "react";
+import { ProjectboxProps } from "./projectBox";
+
+async function getProjects(){
+    const response = await fetch("https://api.github.com/users/jaytan3966/repos", {
+        headers: {
+            'Authorization': `${process.env.GITHUB_TOKEN}`,
+        }
+    });
+    const data = await response.json();
+
+    const techs = [
+        ["ReactJS", "MongoDB", "ExpressJS", "NodeJS"],
+        ["ReactJS", "Spotify API", "YouTube API"],
+        ["NextJS", "TailwindCSS", "Typescript", "MongoDB"],
+        ["ReactJS", "MongoDB", "NodeJS", "Auth0", "Gemini API"],
+        ["NextJS", "TailwindCSS", "Typescript", "Github API"],
+        ["Python", "Terminal"]
+    ];
+
+    const projects = data.map((project : any) => {
+        const date = new Date(project.created_at);
+        const month = String(date.getMonth() + 1).padStart(2, '0'); // "08"
+        const year = date.getFullYear(); // "2024"
+        const formattedDate = `${month}-${year}`;
+        return {
+            name: project.name,
+            description: project.description,
+            date: formattedDate,
+            techs: project.topics,
+            url: project.html_url,
+        }
+    })
+    console.log(projects);
+    return projects;
+}
 
 export default function ProjectsGrid(){
 
-    const count = [1,2,3,4,5,6]
+    const [projects, setProjects] = useState<ProjectboxProps[]>([]);
+    useEffect(() => {
+        const fetchProjects = async () => {
+            const projects = await getProjects();
+            setProjects(projects);
+        };
+        fetchProjects();
+    }, []);
     
+    const techs = [
+        ["ReactJS", "MongoDB", "ExpressJS", "NodeJS"],
+        ["ReactJS", "Spotify API", "YouTube API"],
+        ["NextJS", "TailwindCSS", "Typescript", "MongoDB"],
+        ["ReactJS", "MongoDB", "NodeJS", "Auth0", "Gemini API"],
+        ["NextJS", "TailwindCSS", "Typescript", "Github API"],
+        ["Python", "Terminal"]
+    ];
     return (
-        <div className="grid grid-cols-2 gap-4 md:grid-cols-3">
-            {count.map((val, i) => {
+        <div className="grid grid-cols-2 gap-4 md:grid-cols-3 items-stretch">
+            {projects.map((proj) => {
                 return (
-                    <div key={i}>
-                        <Projectbox/>
+                    <div key={proj.name}>
+                        <Projectbox name={proj.name} date={proj.date} description={proj.description} techs={proj.techs} url={proj.url}/>
                     </div>
                 )
             })}
